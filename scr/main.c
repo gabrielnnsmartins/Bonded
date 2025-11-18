d5d398f28e66df3551f329641da472c4e41c4802
Gabriel Nunes Martins <gabrielnnsmartins@hotmail.com>
2025-11-18 18:10:16 -0300

    Adicionei a pasta src, coloquei uma janela na função main com um loop do jogo

--- scr/main.c
+++ scr/main.c
@@ -4,8 +4,16 @@ int main(){
     constexpr int screenWidth = 800;
     constexpr int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    InitWindow(screenWidth, screenHeight, "Bonded");
     SetTargetFPS(60);
-        
+    
+     while (!WindowShouldClose())
+    {    
+        BeginDrawing();
+            ClearBackground(Bonded/cenario.png);
+        EndDrawing();
+    }
+    
+    CloseWindow();
     return 0;
 }
